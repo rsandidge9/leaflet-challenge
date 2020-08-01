a94209b407bdbddc19efb3f4ee9210d44fdec30b
rsandidge9 <rsandidge9@yahoo.com>
2020-08-01 00:19:26 -0500

    update homework

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,10 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function (response) {
     // Once we get a response, send the data.features object to the createFeatures function
-    console.log(response);
+    // console.log(response);
     createFeatures(response.features);
 });
 
@@ -16,16 +16,61 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     }
+    function styleInfo(feature) {
+        return {
+            color: "white",
+            // Call the chooseColor function to decide which color to color our neighborhood (color based on borou
+            fillColor: colorRange(feature.properties.mag),
+            radius: radiusFunc(feature.properties.mag),
+            fillOpacity: 0.5,
+            weight: 1.5
+        };
 
+    }
+    // Define colors depending on the magnituge of the earthquake
+    function colorRange(magnituge) {
+        switch (true) {
+            case magnituge >= 5.0:
+                return 'red';
+                break;
+            case magnituge >= 4.0:
+                return 'blue';
+                break;
+            case magnituge >= 3.0:
+                return 'purple';
+                break;
+            case magnituge >= 2.0:
+                return 'green';
+                break;
+            case magnituge >= 1.0:
+                return 'pink';
+                break;
+            default:
+                return 'yellow';
+        };
+    }
+    function radiusFunc(mag) {
+        if (mag == 0) {
+            return 1;
+        }
+        return mag * 3;
+    }
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+
         onEachFeature: onEachFeature
-    });
+    }
+    );
+
 
     // Sending our earthquakes layer to the createMap function
-     createMap(earthquakes);
- }
+    createMap(earthquakes);
+}
 
 function createMap(earthquakes) {
     //Define streetmap and darkmap layers
@@ -37,7 +82,7 @@ function createMap(earthquakes) {
         id: "mapbox/streets-v11",
         accessToken: API_KEY
     });
-     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
@@ -56,8 +101,8 @@ function createMap(earthquakes) {
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 5,
+        center: [40.7128, -74.0059],
+        zoom: 3,
         layers: [streetmap, earthquakes]
     });
 
@@ -67,66 +112,46 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
-}
-
-// Create the legend
-var legend = L.control({ position: "bottomright" });
-
-//Add legend
-legend.onAdd = function () {
-    var legend_loc = L.DomUtil.create("div", "info legend"),
-        levels = [0, 1, 2, 3, 4, 5];
 
 
-    // Loop through magnitude intervals and generate a label with a colored square for each interval
-     for (var i = 0; i < data.length; i++) {
-
-    //    Set the data location property to a variable
-         var location = response[i].location;
-
-         if (location) {
-        //   Add a new marker to the cluster group and bind a pop-up
-             markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-                 .bindPopup(response[i].descriptor));
-          }
-      };
-
-
-    // Add legend to the map
-    legend.addTo(myMap);
-
-    // Define colors depending on the magnituge of the earthquake
-     function colorRange(magnituge) {
-
-
-        switch (true) {
-            case magnituge >= 5.0:
-                return 'red';
-                break;
-
-
-            case magnituge >= 4.0:
-                return 'blue';
-                break;
-
-            case magnituge >= 3.0:
-                return 'purple';
-                break;
-
-
-            case magnituge >= 2.0:
-                return 'green';
-                break;
-
-
-            case magnituge >= 1.0:
-                return 'pink';
-                break;
-
-
-            default:
-                return 'yellow';
-         };
-     }
-
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = geojson.options.limits;
+        var colors = geojson.options.colors;
+        var labels = [];
+        // Add min & max
+        var legendInfo = "<h1>Median Income</h1>" +
+            "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+            "</div>";
+        div.innerHTML = legendInfo;
+        limits.forEach(function (limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
     };
+
+
+
+
+    // Create the legend
+    //var legend = L.control({ position: "bottomright" });
+
+    //Add legend
+    // legend.onAdd = function () {
+    // var legend_loc = L.DomUtil.create("div", "info legend");
+    // var level = [0, 1, 2, 3, 4, 5];
+    // var colors = ['red', 'blue', 'purple', 'green', 'pink', 'yellow'];
+    // for (var i = 0; i < level.length; i++) {
+    // div.innerHTML +=
+    // "<i style='background: " + colors[i] + "'></i> " + level[i] + (level[i + 1] ? "&ndash;" + level[i + 1] + "<br>" : "+");
+    // } return div;
+    // }
+};
+
+
+// Add legend to the map
+legend.addTo(myMap);
